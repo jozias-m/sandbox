e9e0eff4e3e41b959edf70d284b9e3958368ae70
Jozias-m <jozias.mijova@gmail.com>
2025-11-23 16:32:22 +0800

    Updated files

--- scripts.js
+++ scripts.js
@@ -2,13 +2,36 @@
 let ip;
 let k = 0;
 
+let darkMode;
+
 function onload() {
-    console.log("");
+    // console.log("");
+
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode === null) {
+        darkMode = true;
+    } else {
+        darkMode = (darkMode === "true");
+    }
+    if (darkMode == true || darkMode == 'true') {
+            document.body.classList.add("darkMode");
+            document.body.classList.remove("lightMode");
+        }else{
+            document.body.classList.add("lightMode");
+            document.body.classList.remove("darkMode");
+        }
 }
 function toggleDarkMode() {
+    if (darkMode == true) {
+        darkMode = false;
+    }else{
+        darkMode = true;
+    }
+    localStorage.setItem("darkMode", darkMode)
     var element = document.body;
     element.classList.toggle("darkMode");
     element.classList.toggle("lightMode");
+    console.log(darkMode);
 }
 
 function sleep(milliseconds) {
