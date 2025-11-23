3fd81f564e80a2b45b40e9071ee39e4b3789bffa
Jozias-m <jozias.mijova@gmail.com>
2025-11-23 15:11:06 +0800

    Updated files

--- scripts.js
+++ scripts.js
@@ -1,7 +1,50 @@
 // Jozias Mijova (c) 2025
+let ip;
+let k = 0;
 
-function toggleDarkMode() {
-   var element = document.body;
-   element.classList.toggle("darkMode");
-   element.classList.toggle("lightMode");
+function onload() {
+    console.log("");
 }
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("darkMode");
+    element.classList.toggle("lightMode");
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+setTimeout(() => {
+    onload()
+}, 10);
+
+async function getIP() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        // document.getElementById('ip').innerText = `Your IP Address: ${data.ip}`;
+        // console.log("IP: " + data.ip);
+        localStorage.setItem("ip", data.ip);
+        console.log("Registered: " + localStorage.getItem("ip"));
+        ip = localStorage.getItem("ip");
+        } 
+    catch (error) {
+        console.error('Error fetching IP address:', error);
+    }
+    return ip;
+}
+
+if (localStorage.getItem("ip") != "0.0.0.0" && localStorage.getItem("ip") != null) {
+    console.log("Welcome back: " + localStorage.getItem("ip"));
+    ip = localStorage.getItem("ip");
+}else{
+    getIP();
+}
+
+let expires = (new Date(Date.now()+ 3091764098999999)).toUTCString();
+document.cookie = "ip=" + localStorage.getItem("ip") + "; expires=" + expires +"; path=/"
